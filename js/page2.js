ed050d09b8d001c50bc676154034cf76a5496c05
sykzen <sykzen@hotmail.com>
2023-03-29 01:13:44 +0200

    changement racine

--- js/page2.js
+++ js/page2.js
@@ -1,11 +1,11 @@
 /* script permettant de recuperer le nombre de joueur selon le nombre de joueur 
     choisi. Une page est renvoyé avec le bon nombre de joueur ainsi que le plateau 
     de jeu affiché à l'écran */
- 
+
 var boutons = document.querySelectorAll("button");
 for (var i = 0; i < boutons.length; i++) {
     boutons[i].addEventListener("click", function() {
         var valeurDuBouton = this.value;
-        window.location.href = "../pageJeu/pageJeu.html?valeur=" + valeurDuBouton;
+        window.location.href = "../html/pageJeu.html?valeur=" + valeurDuBouton;
     });
 }
