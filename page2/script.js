7aed542036e039b9fe63d13f880921fbc219384a
uvsq21905607 <co.brideboisdarcy@gmail.com>
2023-03-28 19:10:54 +0200

    ajout du plateau de jeu et de commentaire.

--- page2/script.js
+++ page2/script.js
@@ -1,5 +1,6 @@
-var boutons = document.querySelectorAll("button");
 
+/* on attribue la valeur a valeurDuBouton selon le bouton et le nombre de joueur choisi*/ 
+var boutons = document.querySelectorAll("button");
 for (var i = 0; i < boutons.length; i++) {
   boutons[i].addEventListener("click", function() {
     var valeurDuBouton = this.value;
@@ -7,6 +8,7 @@ for (var i = 0; i < boutons.length; i++) {
   });
 }
 
+/* fonction permettant de récuperer la valeur du nombre de joueur et renvoyer la page associé avec le bon nombre de joueur et le jeu*/
 function afficherValeur() {
   var valeur = document.getElementById("input-valeur").value;
   window.location.href = "../pageJeu/pageJeu.html?valeurDuBouton=" + encodeURIComponent(valeur);
--- pageJeu/script.js
+++ pageJeu/script.js
@@ -11,14 +11,6 @@ function changerDe() {
     de.src = "../assets/images/de" + resultat + ".png";
   }
 
-
-
-
-
-
-
-
-
 // Créer le code HTML correspondant
 var codeHTML = '';
 for (var i = 1; i <= valeurDuBouton; i++) {
@@ -33,9 +25,27 @@ for (var i = 1; i <= valeurDuBouton; i++) {
   codeHTML +=     '<div class="part part-6"></div>';
   codeHTML +=   '</div>';
   codeHTML +=   '</div>';
-
 }
 
 // Insérer le code HTML dans la page
 document.getElementById('joueurs-container').innerHTML = codeHTML;
-  window.onload = updateValeur;
+
+
+  var codeHTML = '';
+for (var i = 1; i <= valeurDuBouton; i++) {
+  codeHTML += '<div class="rectangle">';
+  codeHTML +=   '<span>Joueur ' + i + '</span>';
+  codeHTML +=   '<div class="camenbert">';
+  codeHTML +=     '<div class="part part-1"></div>';
+  codeHTML +=     '<div class="part part-2"></div>';
+  codeHTML +=     '<div class="part part-3"></div>';
+  codeHTML +=     '<div class="part part-4"></div>';
+  codeHTML +=     '<div class="part part-5"></div>';
+  codeHTML +=     '<div class="part part-6"></div>';
+  codeHTML +=   '</div>';
+  codeHTML +=   '</div>';
+}
+
+// Insérer le code HTML dans la page
+document.getElementById('joueurs-container').innerHTML = codeHTML;
+window.onload = updateValeur;
