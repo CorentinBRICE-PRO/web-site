9cf921496c7f423f2ef893cf830cc3517f821ca1
Ahmed Djeb <ahmed.djeblahi@ens.uvsq.fr>
2023-04-03 19:41:10 +0200

    Connexion de bdd

--- public/js/pageJeu.js
+++ public/js/pageJeu.js
@@ -10,25 +10,23 @@ document.getElementById("nombreJoueur").value = valeurDuBouton;
 
 
 
-// Genere aleatoirement une face d'un dé
-function changerDe() {
-  var de = document.getElementById("de");
-  var resultat = Math.floor(Math.random() * 6) + 1;
-  de.src = "../assets/images/de" + resultat + ".png";
-}
 
 
 
 // Configuration de Firebase
-var firebaseConfig = {
-  apiKey: "AIzaSyAspisnbtuq7zXqNyeFA0xfkRKPCwlKDXk",
-  authDomain: "trivialpoursite.firebaseapp.com",
-  projectId: "trivialpoursite",
-  storageBucket: "trivialpoursite.appspot.com",
-  messagingSenderId: "934201717719",
-  appId: "1:934201717719:web:77188c864e7bc443085192",
-  measurementId: "G-KF17Q422EM"
+require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
+// Utilisez firebaseConfig pour initialiser votre connexion Firestore
+
 
 // Initialise l'application Firebase
 firebase.initializeApp(firebaseConfig);
@@ -110,4 +108,23 @@ document.getElementById('joueurs-container').innerHTML = codeHTML;
     console.log("Erreur lors de la récupération du document : ", error);
   });
 
+
+// Genere aleatoirement une face d'un dé
+function changerDe() {
+  var de = document.getElementById("de");
+  var resultat = Math.floor(Math.random() * 6) + 1;
+
+  firebase.firestore().collection('partie').doc('Partie1').update({
+    de: resultat
+    })
+
+    .then(() => {
+        console.log("Le champ 'nbjoueurs' a été mis à jour avec succès !");
+    })
+    .catch((error) => {
+        console.error("Erreur lors de la mise à jour du champ 'nbjoueurs' : ", error);
+    });
+
+  de.src = "../assets/images/de" + resultat + ".png";
+}
   window.onload = updateValeur;
