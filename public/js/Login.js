bf2e1750b90526560d24354b5a6ed1415c631665
Rafifoulol <raph07.guerin@gmail.com>
2023-04-10 18:08:35 +0200

    Fin de la Connexion

--- public/js/Login.js
+++ public/js/Login.js
@@ -14,8 +14,11 @@ measurementId: "G-KF17Q422EM"
 //Initialisation de Firebase
 firebase.initializeApp(firebaseConfig);
 
-var alertbox = document.getElementById("alert");
-alertbox.style.display = "none";
+var alertSignUp = document.getElementById("alert-signup");
+alertSignUp.style.display = "none";
+
+var alertLogIn = document.getElementById("alert-login");
+alertLogIn.style.display = "none";
 
 // Genere un id de game aleatire de 7 caracteres 
 function generationIdgameAleatoire() {
@@ -30,7 +33,7 @@ function generationIdgameAleatoire() {
 
 // Création d'un compte dans la base de donnée
 function signUpJoueur() {
-    alertbox.style.display = "none";
+    alertSignUp.style.display = "none";
 
     var Pseudo = document.getElementById("Pseudo").value;
     var Email = document.getElementById("email-signup").value;
@@ -46,67 +49,86 @@ function signUpJoueur() {
                 docref.onSnapshot((doc) => {
                     // Affiche un message d'erreur.
                     //console.log("Le compte exite déjà !!!");
-                    alertbox.firstChild.textContent = "Ce Pseudo existe déja. Veuillez réessayer.";
-                    alertbox.style.display = "block";
+                    alertSignUp.firstChild.textContent = "Ce Pseudo existe déja. Veuillez réessayer.";
+                    alertSignUp.style.display = "block";
                 });
             } else {
-                // Vérifie si il y a un champ mot de Passe et Email
-                if ( Email && MotDePasse) {
-                    // Atribution de toutes les informations tapé par l'utilisateur
-                    firebase.firestore().collection("Login").doc(Pseudo).set({
-                        email: Email,
-                        mdp: MotDePasse,
-                        pseudo: Pseudo,
-                        idgame : generationIdgameAleatoire(),
-                    })
-                    .then(() => {
-                    //Redirection vers la page principale
-                    console.log("Compte créer avec succès !");
-                    window.location.href = "../index.html";
-                    })
-                    .catch((error) => {
-                    console.error("Erreur lors de l'ajout du document :", error);
-                    });
+                // Vérifie si il y a un champ Mot de Passe et Email
+                if (validateEmail(Email)) {
+                    if (MotDePasse) {
+                        // Atribution de toutes les informations tapé par l'utilisateur
+                        firebase.firestore().collection("Login").doc(Pseudo).set({
+                            email: Email,
+                            mdp: MotDePasse,
+                            pseudo: Pseudo,
+                            idgame : generationIdgameAleatoire(),
+                        })
+                        .then(() => {
+                        //Redirection vers la page principale
+                        console.log("Compte créer avec succès !");
+                        window.location.href = "../index.html";
+                        })
+                        .catch((error) => {
+                        console.error("Erreur lors de l'ajout du document :", error);
+                        });
+                    } else {
+                        // Affiche un message d'erreur
+                        alertSignUp.firstChild.textContent = "Vous n'avez pas renseigné de Mot de Passe";
+                        alertSignUp.style.display = "block";
+                    }
                 } else {
                     // Affiche un message d'erreur
-                    alertbox.firstChild.textContent = "Vous n'avez pas renseigné votre Email ou Mot de passe";
-                    alertbox.style.display = "block";
+                    alertSignUp.firstChild.textContent = "Email Incorrect";
+                    alertSignUp.style.display = "block";
                 }
                 
             }
         })
     } else {
         // Affiche un autre message d'erreur.
-        alertbox.firstChild.textContent = "Vous n'avez pas mis de Pseudo";
-        alertbox.style.display = "block";
+        alertSignUp.firstChild.textContent = "Vous n'avez pas mis de Pseudo";
+        alertSignUp.style.display = "block";
     } 
 }
 
 // Connexion a une partie deja existante dans la bd
 function loginJoueur() {
-    var idJoueursaisie = document.getElementById("email").value;
-    var idmdp = document.getElementById("mdp").value;
+    var Email = document.getElementById("email-login").value;
+    var idmdp = document.getElementById("mdp-login").value;
+
+    alertLogIn.style.display = "none";
+
     //Verifie si l'idgame saisie existe et si cest le cas charge la page de jeu adequate
     firebase.firestore().collection("Login").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          if (doc.exists && doc.id === idJoueursaisie) {
-            if (doc.data().mdp === idmdp) {
-            const idgame = doc.data().idgame;
-            window.location.href = "../html/pageJeu.html?valeur=" + idgame ;
-            console.log("Connexion reussi ", idgame);
-            }
-            else{
-                console.log("Mdp Incorrect ");
-
+        if (Email) {
+            if (doc.exists && doc.get("email") === Email) {
+                if (doc.get("mdp") === idmdp) {
+                    const idgame = doc.data().idgame;
+                    window.location.href = "../html/pageJeu.html?valeur=" + idgame ;
+                    console.log("Connexion reussi ", idgame);
+                }
+                else {
+                    alertLogIn.firstChild.textContent = "Mot de passe Inconnu ou Incorrect";
+                    alertLogIn.style.display = "block";
+                }
+            } else {
+                alertLogIn.firstChild.textContent = "Adresse Email Inconnu ou Incorrect";
+                alertLogIn.style.display = "block";
             }
         } else {
-           
-            console.log("identifiant inconnue");
+            alertLogIn.firstChild.textContent = "Vous n'avez pas entré d'adresse Email";
+            alertLogIn.style.display = "block";
         }
+          
     });
+
     }).catch((error) => {
         console.log("Erreur lors de la récupération du document : ", error);
     });
 }
 
-
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
